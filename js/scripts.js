0381da0eb8a194ea09041adb44125d5a8bfb1eb5
fglukhov <f.glukhov@gmail.com>
2014-05-14 16:06:22 +0200

    Ver. 2.4
    Pros popups added

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,34 @@ $(window).scroll(function() {
 
 $(document).ready(function () {
 
+	// Наши преимущества, попапы
+	
+	$(".pros-item .descr").click(function() {
+		var tooltip = $(this).parents(".pros-item").find(".pros-tooltip");
+		
+		$(".pros-item .pros-tooltip").hide();
+		
+		tooltip.fadeIn(200);
+		
+	});
+	
+	$(".pros-tooltip .close").click(function() {
+		$(this).parents(".pros-tooltip").fadeOut(150);
+	});
+	
+	$(document).mouseup(function (e) {
+    var container1 = $(".pros-tooltip");
+    var container2 = $(".pros-item .descr");
+
+    if (!container1.is(e.target) && !container2.is(e.target) // if the target of the click isn't the container...
+        && container1.has(e.target).length === 0 && container2.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container1.fadeOut(150)
+    }
+  });
+
+	// Наши преимущества, попапы END
+	
   $(".tabbed-content").each(function() {
     var tabs = $(this).children(".tabs").find(".tab");
     var tabContents = $(this).children(".tabs-content").children(".tab-content");
